86bd0101c86fc2b81fd4a8d52ed5c3e68861a110
agin656 <agin656@gmail.com>
2016-04-20 13:30:45 -0400

    deleted an unneeded nested folder

--- qwopper-master/qwopper-master/src/com/slowfrog/qwop/IFilter.java
+++
@@ -1,5 +0,0 @@
-package com.slowfrog.qwop;
-
-public interface IFilter<T> {
-  boolean matches(T t);
-}
--- qwopper-master/qwopper-master/src/com/slowfrog/qwop/Log.java
+++
@@ -1,9 +0,0 @@
-package com.slowfrog.qwop;
-
-public interface Log {
-  public void log(String message);
-
-  public void log(String message, Throwable e);
-
-  public void logf(String format, Object... args);
-}
